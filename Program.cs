1a3abda8948e944aaf85fdcfc63e0003f2824a12
pepes1234 <lucas.pepes07@gmail.com>
2022-11-16 13:05:38 -0300

    tudo

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+
+string text = File.ReadAllText("Ronnie O'Sullivan Fastest 147.m3u8");
+var textFile = File.ReadAllLines("Ronnie O'Sullivan Fastest 147.m3u8");
+
+for(int i = 6; i < textFile.Count(); i+=2)
+{
+    if(i == textFile.Count() - 1)
+        break;
+    text = text.Replace(i.ToString(), "Ronnie O'Sullivan Fastest 147:" + i.ToString());
+}
+File.WriteAllText("Ronnie O'Sullivan Fastest 147.m3u8", text);
+    
